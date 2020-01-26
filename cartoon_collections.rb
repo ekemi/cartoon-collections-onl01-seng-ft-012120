50e19bbf6b6b8d205f49a255a8ad45ee54fd7be8
poignant-writer-1163 <edmundopierre@yahoo.com>
2020-01-26 06:32:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,6 +19,6 @@ end
 def find_the_cheese (array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  puts (array&cheese_types).join
+  (array&cheese_types).join
          
 end
